ddff84a2118a6e4cdb37017071cdba4be45caf76
ChenXing <renzhendian@gmail.com>
2017-07-26 14:49:48 +0800

    代码整理

--- src/cxing/zookeeper/ZkDemo.java
+++ src/cxing/zookeeper/ZkDemo.java
@@ -1,12 +1,15 @@
 package cxing.zookeeper;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 public class ZkDemo {
 
@@ -14,26 +17,61 @@ public class ZkDemo {
 		ZooKeeper zk = null;
 		zk = new ZooKeeper("localhost:2181",6000, null);
 		String path = "/user01";
-		zk.delete(path, -1);
-		zk.create(path,null,ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
-		String subPath01="/user01/s01";
-		byte[] data1 = "test11".getBytes();
-		zk.create(subPath01,data1,ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL);
-		String subPath02="/user01/s02";
-		byte[] data2 = "test22".getBytes();
-		zk.create(subPath02,data2,ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL);
-		List<String> nodeList01 = zk.getChildren(path, false);
-        for (String node : nodeList01) {
-            byte[] b = zk.getData(path + "/" + node,false,null); 
-            System.out.println(new String(b));
-        }
-        System.out.println("*********************************");
-		zk.delete(subPath01, -1);
-		List<String> nodeList02 = zk.getChildren(path, false);
-        for (String node : nodeList02) {
-            byte[] b = zk.getData(path + "/" + node,false,null); 
-            System.out.println(new String(b));
-        }
+		ZkDemo zkDemo=new ZkDemo();
+		//清空以前的残留数据
+		zkDemo.deleteNode(zk, path);
+		//创建根路径
+		zkDemo.createNode(zk, path, null);
+		//两个子路径
+		zkDemo.createNode(zk, "/user01/s01", "sub01");
+		zkDemo.createNode(zk, "/user01/s02", "sub02");
+		zkDemo.readNode(zk, path);
+        //修改
+		zkDemo.updateNode(zk, "/user01/s02", "sub02修改");		
+		zkDemo.readNode(zk, path);
+		//删除
+		zkDemo.deleteNode(zk, "/user01/s02");
+		zkDemo.readNode(zk, path);
 	}
 	
+	public String createNode(ZooKeeper zk,String path,String data) throws KeeperException, InterruptedException{
+		byte[] bts=null;
+		if(data!=null) bts=data.getBytes();
+		String actualPath=zk.create(path,bts,ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+	    return actualPath;
+	}
+	
+	public void updateNode(ZooKeeper zk,String path,String data) throws KeeperException, InterruptedException{
+		byte[] bts=null;
+		if(data!=null) bts=data.getBytes();
+		Stat stat = zk.exists(path, true);
+		if(stat!=null){
+			zk.setData(path, bts, -1);
+		}
+	}
+	
+	public void deleteNode(ZooKeeper zk,String path) throws KeeperException, InterruptedException{
+		Stat stat = zk.exists(path, true);
+		if(stat!=null){
+			List<String> nodeList = zk.getChildren(path, false);
+	        for (String node : nodeList) {
+	        	zk.delete(path + "/" + node, -1);
+	        }
+	        zk.delete(path, -1);
+		}
+	}
+	
+	public Map<String,String> readNode(ZooKeeper zk,String path) throws KeeperException, InterruptedException{
+		System.out.println("...读取：");
+		Map<String,String> map=new HashMap<String,String>();
+		List<String> nodeList01 = zk.getChildren(path, false);
+        for (String node : nodeList01) {
+        	String p=path + "/" + node;
+            byte[] b = zk.getData(p,false,null); 
+            String d=new String(b);
+            map.put(p, d);
+            System.out.println(d);
+        }
+        return map;
+	}
 }
